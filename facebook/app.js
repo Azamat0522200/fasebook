09ec53e8ddd3b660d60f716b109466fecfc79519
Azamat0522200 <155151535+Azamat0522200@users.noreply.github.com>
2023-12-29 15:27:13 +0500

    Add files via upload

---
+++ facebook/app.js
@@ -0,0 +1,327 @@
+const navbaritem2 = document.querySelector(".navbar-item2");
+const navbaritem3 = document.querySelector(".navbar-item3");
+const navbaritem4 = document.querySelector(".navbar-item4");
+const navbaritem5 = document.querySelector(".navbar-item5");
+const navbaritem6 = document.querySelector(".navbar-item6");
+const navbaritem7 = document.querySelector(".navbar-item7");
+const navbaritem8 = document.querySelector(".navbar-item8");
+const navbaritem9 = document.querySelector(".navbar-item9");
+const topish = document.querySelector("#topish");
+const arrow = document.querySelector("#arrow");
+const search = document.querySelector(".search");
+const gass = document.querySelector("#gass");
+const gass1 = document.querySelector("#gass1");
+const gass2 = document.querySelector("#gass2");
+const gass3 = document.querySelector("#gass3");
+const gass4 = document.querySelector("#gass4");
+const container = document.querySelector(".container");
+const dos = document.querySelector(".dos");
+const dos1 = document.querySelector(".dos1");
+const dos2 = document.querySelector(".dos2");
+const dos3 = document.querySelector(".dos3");
+const dos4 = document.querySelector(".dos4");
+const dos5 = document.querySelector(".dos5");
+const dos6 = document.querySelector(".dos6");
+const dos7 = document.querySelector(".dos7");
+const dos8 = document.querySelector(".dos8");
+const dos9 = document.querySelector(".dos9");
+const tashlash9 = document.querySelector(".tashlash9");
+const tashlash8 = document.querySelector(".tashlash8");
+const tashlash7 = document.querySelector(".tashlash7");
+const tashlash6 = document.querySelector(".tashlash6");
+const tashlash5 = document.querySelector(".tashlash5");
+const tashlash4 = document.querySelector(".tashlash4");
+const tashlash3 = document.querySelector(".tashlash3");
+const tashlash2 = document.querySelector(".tashlash2");
+const tashlash1 = document.querySelector(".tashlash1");
+const tashlash = document.querySelector(".tashlash");
+const obuna = document.querySelector(".obuna");
+const menu = document.querySelector(".menu");
+const houe = document.querySelector(".houe");
+const house5 = document.querySelector("#house5");
+const tv5 = document.querySelector("#tv5");
+const user5 = document.querySelector("#user5");
+const shop5 = document.querySelector("#shop5");
+const bell5 = document.querySelector("#bell5");
+const obunabolish = document.querySelector(".obunabolish");
+const bars = document.querySelector("#bars");
+const bars1 = document.querySelector("#bars1");
+const bars2 = document.querySelector("#bars2");
+const bars3 = document.querySelector("#bars3");
+const bars4 = document.querySelector("#bars4");
+const qongiroq = document.querySelector(".qongiroq");
+const shop4 = document.querySelector("#shop4");
+const house4 = document.querySelector("#house4");
+const tv4 = document.querySelector("#tv4");
+const bell1 = document.querySelector("#bell1");
+const bell = document.querySelector("#bell");
+const bell2 = document.querySelector("#bell2");
+const bell3 = document.querySelector("#bell3");
+const bell4 = document.querySelector("#bell4");
+const user4 = document.querySelector("#user4");
+const tv2 = document.querySelector("#tv2");
+const user3 = document.querySelector("#user3");
+const user = document.querySelector("#user");
+const user1 = document.querySelector("#user1");
+const house2 = document.querySelector("#house2");
+const house3 = document.querySelector("#house3");
+const tv = document.querySelector("#tv");
+const house1 = document.querySelector("#house1");
+const shop = document.querySelector("#shop");
+const shop1 = document.querySelector("#shop1");
+const shop2 = document.querySelector("#shop2");
+const shop3 = document.querySelector("#shop3");
+const i7 = document.querySelector("#i7");
+const i = document.querySelector("#i");
+const i1 = document.querySelector("#i1");
+const i2 = document.querySelector("#i2");
+const i3= document.querySelector("#i3");
+const i4 = document.querySelector("#i4");
+const i5 = document.querySelector("#i5");
+const i6 = document.querySelector("#i6");
+tv3.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "block";
+    obunabolish.style.display = "none";
+    houe.style.display = "none";
+})
+shop2.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "none";
+    obunabolish.style.display = "none";
+    houe.style.display = "block";
+})
+shop3.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "none";
+    obunabolish.style.display = "none";
+    houe.style.display = "block";
+})
+shop.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "none";
+    obunabolish.style.display = "none";
+    houe.style.display = "block";
+})
+shop1.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "none";
+    obunabolish.style.display = "none";
+    houe.style.display = "block";
+})
+tv3.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "block";
+    obunabolish.style.display = "none";
+    houe.style.display = "none";
+})
+user.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "none";
+    obunabolish.style.display = "block";
+})
+user1.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "none";
+    obunabolish.style.display = "block";
+})
+user3.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "none";
+    houe.style.display = "none";
+    obunabolish.style.display = "block";
+})
+house3.addEventListener("click",() =>{
+    container.style.display = "block";
+    obuna.style.display = "none";
+    houe.style.display = "none";
+    obunabolish.style.display = "none";
+})
+tv2.addEventListener("click",() =>{
+    container.style.display = "none";
+    obunabolish.style.display = "none";
+    obuna.style.display = "block";
+})
+house2.addEventListener("click",() =>{
+    container.style.display = "block";
+    obunabolish.style.display = "none";
+})
+tv.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "block";
+})
+house1.addEventListener("click",() =>{
+    container.style.display = "block";
+    obuna.style.display = "none";
+})
+tv.addEventListener("click",() =>{
+    container.style.display = "none";
+    obuna.style.display = "block";
+})
+bell.addEventListener("click",() =>{
+    container.style.display = "none";
+     qongiroq.style.display = "block";
+})
+bell1.addEventListener("click",() =>{
+    obuna.style.display = "none";
+     qongiroq.style.display = "block";
+})
+bell2.addEventListener("click",() =>{
+    obunabolish.style.display = "none";
+     qongiroq.style.display = "block";
+})
+bell3.addEventListener("click",() =>{
+     houe.style.display = "none";
+     qongiroq.style.display = "block";
+})
+house4.addEventListener("click",() =>{
+    container.style.display = "block";
+     qongiroq.style.display = "none";
+})
+tv4.addEventListener("click",() =>{
+    obuna.style.display = "block";
+     qongiroq.style.display = "none";
+})
+user4.addEventListener("click",() =>{
+    obunabolish.style.display = "block";
+     qongiroq.style.display = "none";
+})
+shop4.addEventListener("click",() =>{
+    houe.style.display = "block";
+     qongiroq.style.display = "none";
+})
+house5.addEventListener("click",() =>{
+    container.style.display = "block";
+     menu.style.display = "none";
+})
+bars.addEventListener("click",() =>{
+    container.style.display = "none";
+     menu.style.display = "block";
+})
+tv5.addEventListener("click",() =>{
+     obuna.style.display = "block";
+     menu.style.display = "none";
+})
+bars1.addEventListener("click",() =>{
+     obuna.style.display = "none";
+     menu.style.display = "block";
+})
+user5.addEventListener("click",() =>{
+    obunabolish.style.display = "block";
+    menu.style.display = "none";
+})
+bars2.addEventListener("click",() =>{
+    obunabolish.style.display = "none";
+    menu.style.display = "block";
+})
+shop5.addEventListener("click",() =>{
+    houe.style.display = "block";
+    menu.style.display = "none";
+})
+
+bars3.addEventListener("click",() =>{
+    houe.style.display = "none";
+    menu.style.display = "block";
+})
+bell5.addEventListener("click",() =>{
+    qongiroq.style.display = "block";
+    menu.style.display = "none";
+})
+
+bars4.addEventListener("click",() =>{
+    qongiroq.style.display = "none";
+    menu.style.display = "block";
+})
+
+tashlash.addEventListener("click",() =>{
+    dos.style.display = "none";
+})
+
+tashlash1.addEventListener("click",() =>{
+    dos1.style.display = "none";
+})
+
+tashlash2.addEventListener("click",() =>{
+    dos2.style.display = "none";
+})
+
+tashlash3.addEventListener("click",() =>{
+    dos3.style.display = "none";
+})
+
+tashlash4.addEventListener("click",() =>{
+    dos4.style.display = "none";
+})
+
+tashlash5.addEventListener("click",() =>{
+    dos5.style.display = "none";
+})
+
+tashlash6.addEventListener("click",() =>{
+    dos6.style.display = "none";
+})
+
+tashlash7.addEventListener("click",() =>{
+    dos7.style.display = "none";
+})
+
+tashlash8.addEventListener("click",() =>{
+    dos8.style.display = "none";
+})
+
+tashlash9.addEventListener("click",() =>{
+    dos9.style.display = "none";
+})
+i.addEventListener("click",() =>{
+    navbaritem2.style.display = "none";
+})
+i1.addEventListener("click",() =>{
+    navbaritem3.style.display = "none";
+})
+i2.addEventListener("click",() =>{
+    navbaritem4.style.display = "none";
+})
+i3.addEventListener("click",() =>{
+    navbaritem5.style.display = "none";
+})
+i4.addEventListener("click",() =>{
+    navbaritem6.style.display = "none";
+})
+i5.addEventListener("click",() =>{
+    navbaritem7.style.display = "none";
+})
+i6.addEventListener("click",() =>{
+    navbaritem8.style.display = "none";
+})
+i7.addEventListener("click",() =>{
+    navbaritem9.style.display = "none";
+})
+
+topish.addEventListener("click",() =>{
+    search.style.display = "block";
+    container.style.display = "none";
+})
+arrow.addEventListener("click",() =>{
+    search.style.display = "none";
+    container.style.display = "block";
+})
+gass.addEventListener("click",() =>{
+    search.style.display = "block";
+    menu.style.display = "none";
+})
+gass.addEventListener("click",() =>{
+    search.style.display = "none";
+    menu.style.display = "block";
+})
+gass.addEventListener("click",() =>{
+    search.style.display = "block";
+    menu.style.display = "none";
+})
+gass1.addEventListener("click",() =>{
+    search.style.display = "block";
+    qongiroq.style.display = "none";
+})
+gass1.addEventListener("click",() =>{
+    search.style.display = "none";
+    qongiroq.style.display = "block";
+})
